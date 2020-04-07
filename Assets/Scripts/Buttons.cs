f5260bc7a22ee378a64d709f4d2d943279218e59
Kyle Fowler <kylespell101@gmail.com>
2020-04-07 15:41:42 -0700

    Hourly check, added buttons script and started grid spawning

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    public GameObject grid;
+
+    private GameObject elementToSpawn;
+    public void AddElement(GameObject toSpawn)
+    {
+        elementToSpawn = toSpawn;
+        Instantiate(elementToSpawn, grid.transform);
+    }
+}
